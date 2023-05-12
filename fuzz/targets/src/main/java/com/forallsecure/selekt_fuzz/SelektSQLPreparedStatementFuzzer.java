cf83c32d544b12919d47dd21f6f429ac4db58afa
Alexander Brewer <abrewer@forallsecure.com>
2023-05-12 15:08:51 -0400

    Initial broken Mayhem integration

---
+++ fuzz/targets/src/main/java/com/forallsecure/selekt_fuzz/SelektSQLPreparedStatementFuzzer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Bloomberg Finance L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.forallsecure.selekt_fuzz;
+
+import com.bloomberg.selekt;
+import org.mockito.invocation.InvocationOnMock;
+//import org.mockito.kotlin.any
+//import org.mockito.kotlin.doAnswer
+//import org.mockito.kotlin.doReturn
+//import org.mockito.kotlin.eq
+//import org.mockito.kotlin.mock
+//import org.mockito.kotlin.times
+//import org.mockito.kotlin.verify
+//import org.mockito.kotlin.whenever
+//import org.mockito.stubbing.Answer
+//import kotlin.test.assertEquals
+//import kotlin.test.assertFailsWith
+//import kotlin.test.assertFalse
+//import kotlin.test.assertTrue
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+
+private static final long POINTER = 42L;
+
+public class SelektSQLPreparedStatementFuzzer {
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    SQLiteDatabase mydb = mock<SQLite>();
+    SQLPreparedStatement(POINTER, data, mydb, CommonThreadLocalRandom);
+  }
+}
